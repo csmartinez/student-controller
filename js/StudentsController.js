6449949b9326fd3ff715859c1b65dbaa86b42802
Carli Martinez and Sarah Joy Calpo <sajoy.pc@gmail.com>
2015-02-23 10:20:36 -0800

    add CRUD functionality

--- js/StudentsController.js
+++ js/StudentsController.js
@@ -1,9 +1,11 @@
 studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
-  $scope.students = [
-    {name: 'Sam Schmidt'},
-    {name: 'Jessica Martian'},
-    {name: 'Sandy Smith'},
-    {name: 'Ryan Rye'},
-    {name: 'Brentwood Forest'}
-  ]
+  $scope.students = [];
+  $scope.addStudent = function() {
+    $scope.students.push({ name: $scope.studentName });
+    $scope.studentName = null;
+  };
+  $scope.deleteStudent = function(student) {
+    var index = $scope.students.indexOf(student);
+    $scope.students.splice(index, 1);
+  };
 });
